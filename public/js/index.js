b704d3d13994cd1d69df0740666b140f6933061e
Van Le <70436051+iRok166@users.noreply.github.com>
2021-12-27 14:32:16 +0700

    hbs helper, advanced search form

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,36 @@
 $(document).ready(function () {
-    new URLSearchParams(window.location.search).forEach((value, key) => {
-      //populate selected select box
-      if (key.includes("sortBy")) {
-        $(`select[name="sortBy"] option:contains(${value})`).prop({
-          selected: true,
-        });
-      }
-  
-      //populate checkbox brands & colors list
-      if (key.includes("brand")) {
-        $(
-          `div.card-body input[type='checkbox'][name='brand'][value="${value}"]`
-        ).prop({
-          checked: true,
-        });
-      }
-      if (key.includes("color")) {
-        $(
-          `div.card-body input[type='checkbox'][name='color'][value="${value}"]`
-        ).prop({
-          checked: true,
-        });
-      }
-    });
-  
-    //clear checked checkbox
-    $("a").click(function (e) {
-      if (e.target.closest(".btn-danger").classList.contains("btn-danger")) {
-        e.preventDefault();
-        $(`div.card-body input[type='checkbox'][name='${this.id}']`).prop({
-          checked: false,
-        });
-      }
-    });
+  new URLSearchParams(window.location.search).forEach((value, key) => {
+    //populate selected select box
+    if (key.includes("sortBy")) {
+      $(`select[name="sortBy"] option:contains(${value})`).prop({
+        selected: true,
+      });
+    }
+
+    //populate checkbox brands & colors list
+    if (key.includes("brand")) {
+      $(
+        `div.card-body input[type='checkbox'][name='brand'][value="${value}"]`
+      ).prop({
+        checked: true,
+      });
+    }
+    if (key.includes("color")) {
+      $(
+        `div.card-body input[type='checkbox'][name='color'][value="${value}"]`
+      ).prop({
+        checked: true,
+      });
+    }
   });
-  
+
+  //clear checked checkbox
+  $("a").click(function (e) {
+    if (e.target.closest(".btn-danger")?.classList.contains("btn-danger")) {
+      e.preventDefault();
+      $(`div.card-body input[type='checkbox'][name='${this.id}']`).prop({
+        checked: false,
+      });
+    }
+  });
+});
