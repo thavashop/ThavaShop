d1ce9012f9cf47b22f4127997e28446463b2963f
Van Le <70436051+iRok166@users.noreply.github.com>
2021-12-31 21:20:48 +0700

    cmt ajax update

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,13 +1,20 @@
 $('#comment-form button').on('click', function (e) {
     e.preventDefault();
-    $.post('/category/post-comment', {
-        content: $("#comment-content").val(),
-        productId: $("#product-id").val(),
-        name: $("#name").val(),
-    }, function (data) {
-        const commentTemplate = Handlebars.compile(
-            document.getElementById("comment-template").innerHTML);
-        const commentHtml = commentTemplate(data);
-        $("#comment-list").prepend(commentHtml);
-    })
+    if ($("#comment-content").val() != "") {
+        $.post('/category/post-comment', {
+            content: $("#comment-content").val(),
+            productId: $("#product-id").val(),
+            name: $("#name").val(),
+        }, function (data) {
+            const commentTemplate = Handlebars.compile(
+                document.getElementById("comment-template").innerHTML);
+            const commentHtml = commentTemplate(data);
+            $("#comment-list").prepend(commentHtml);
+            $("#comment-content")[0].value = '';
+            let numberComment = $("#comments-size")[0].innerHTML;
+            let words = numberComment.split(" ");
+            words[0]++;
+            $("#comments-size")[0].innerHTML = `${words[0]} comments`;
+        })
+    }
 })
