183b5765a4b837ca5f9c83282be02452a2f8d85d
Van Le <70436051+iRok166@users.noreply.github.com>
2022-01-10 00:37:54 +0700

    fix cart ajax

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,6 +2,7 @@
 const cookieData = $.cookie("cart");
 const cartLength = $.cookie("cartLength");
 let cart;
+let total = 0;
 
 //set timeout to debounce the function
 let timeout;
@@ -9,23 +10,31 @@ if (cookieData) {
   cart = JSON.parse(cookieData.slice(2));
 }
 
-const inputQuantity = $(".table-responsive input[type='number']");
-const beforeQuantity =
-  Number(inputQuantity.val()) < 0 ? 0 : Number(inputQuantity.val());
+const inputNumEl = $(".table-responsive input[type='number']");
 
 //assign cookie cart length to cart length
 $("#basket-overview")
   .find("span")
   .text(cartLength ?? 0);
 
-inputQuantity.bind("input", function () {
+inputNumEl.bind("input", function () {
   const self = this;
   const productId = $(self).attr("data-id");
+  const beforeQuantity = this.getAttribute("data-quantity");
 
   //assign the value of the input to the quantity variable
-  $(`.table-responsive #${productId}`).text(
-    "$" + (inputQuantity.val() * inputQuantity.attr("data-price")).toFixed(2)
+  $(.table-responsive #${productId}).text(
+    "$" + (self["value"] * self.getAttribute("data-price")).toFixed(2)
   );
+
+  $(".table-responsive .totalEach").each(function (index, el) {
+    total += Number($(el).text().split("$")[1]);
+  });
+
+  $(".table-responsive #total").text("$" + total.toFixed(2));
+
+  //tao muốn chơi game
+  total = 0;
 
   clearTimeout(timeout);
   timeout = setTimeout(function () {
@@ -38,6 +47,10 @@ inputQuantity.bind("input", function () {
     }
 
     updateCart(productId, afterQuantity);
+    self.setAttribute(
+      "data-quantity",
+      Number($(self).val()) < 0 ? 0 : Number($(self).val())
+    );
   }, 1000);
 });
 
@@ -46,9 +59,13 @@ const updateCart = (productId, quantity) => {
     type: "POST",
     url: "/cart",
     data: { productId, quantity },
-    success: function (data) {},
+    success: function (data) {
+      console.log("Post cart");
+    },
     error: function (xhr, status, err) {
       console.log(err);
     },
   });
 };
+Viết cho Huỳnh Hữu Khánh
+Aa
