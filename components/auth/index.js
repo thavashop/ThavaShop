b05932498b3feeccc55900f89a0efec8b02d47db
Van Le <70436051+iRok166@users.noreply.github.com>
2022-01-13 20:15:36 +0700

    fix bug

--- components/auth/index.js
+++ components/auth/index.js
@@ -10,8 +10,8 @@ router.post("/login", authController.authentication);
 router.get("/logout", authController.logout);
 router.post("/register", authController.register);
 router.get("/register-success", function (req, res) {
-  const { email } = req.query
-  res.render('auth/views/activate', {email})
+  const { email, username } = req.query;
+  res.render("auth/views/activate", { email, username });
 });
 
 router.get("/account", loggedInUserGuard, (req, res) => {
@@ -21,13 +21,13 @@ router.post("/account", authController.editAccount);
 router.post("/account/password", authController.changePassword);
 
 router.get("/activate", authController.activate);
-router.post("/resendEmail", authController.resendEmail)
+router.post("/resendEmail", authController.resendEmail);
 
 router.get("/forgot-password", (req, res) => {
   res.render("auth/views/forgotPassword");
-})
-router.post("/forgot-password", authController.sendMailForgotPassword)
-router.get("/reset-password", authController.resetPassword)
-router.post("/update-password", authController.updatePassword)
+});
+router.post("/forgot-password", authController.sendMailForgotPassword);
+router.get("/reset-password", authController.resetPassword);
+router.post("/update-password", authController.updatePassword);
 
 module.exports = router;
